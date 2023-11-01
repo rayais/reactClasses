3719618abdd1db19af340d824d68755ef310522f
rayais <rayen.ben.mansour@gmail.com>
2023-11-01 10:01:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class  App extends Component {
+  constructor()
+    {
+      super();
+      this.state={
+        fullName:"C.ronaldo",
+        bio:"Cristiano Ronaldo is a professional soccer player who has set records while playing for the Manchester United, Real Madrid and Juventus clubs, as well as the Portuguese national team.", 
+        imgSrc:"https://hips.hearstapps.com/hmg-prod/images/gettyimages-971463110.jpg?resize=1200:*", 
+        profession:"Professional coccer player",
+        show:false
+      }
+    }
+    updateshow ()
+    {
+      
+      //console.log(this.state.show);
+      this.state.show?this.setState({show:false}):this.setState({show:true})
+    }
+  render(){
+  
+    return (
+      <div className="App">
+        <button onClick={()=>this.updateshow()}>Show</button>
+        {this.state.show && (
+          <div className='card'>
+            <h1>{this.state.fullName}</h1>
+            <div className='incard'>
+              <img src={this.state.imgSrc} alt={this.state.fullName}/>
+              <p>{this.state.bio}</p>
+            </div>
+            <h3>{this.state.profession}</h3>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
